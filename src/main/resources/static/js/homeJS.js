66a3573bb2ad8f9d0da82b5ab3d7805f9cd426c9
Mikołaj <mikolaj.gronek@gmail.com>
2019-11-04 21:50:45 +0100

    Dodano przyciski

---
+++ src/main/resources/static/js/homeJS.js
@@ -0,0 +1,11 @@
+
+function multi(val)
+{
+    document.getElementById("d").value = document.getElementById("d").value * val;
+}
+
+function myFunction() {
+    document.getElementById("d").value = 2;
+    // alert(new Date());
+
+}
